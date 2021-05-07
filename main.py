a029b6f40cc416493a9cc9b4439271eac1507d96
V. Fabian T. Mendes C <>
2021-05-07 22:30:18 +0000

    Almost, done. adquisition from episodes list/dictionary done. Now it's necessary to try to export it into a file and then to xpsf (for VLC playlist)

--- main.py
+++ main.py
@@ -39,22 +39,10 @@ for i in range(len(main_input)):
 # ..dictionary.
 
 
-def createDict(lista):
-
-    #	creates dictionary:
-    dict_line = {}
-    aux = []
-    for i in range(len(lista)):
-        aux = lista[i].split('=')
-        aux[1] = aux[1].replace('"', '')
-
-        lista[aux[0]] = aux[1]
-    return lista
-
-
+'''
 def cleanLink(raw_list):
-    '''raw_ soup.find("
-    '''
+    #raw_ soup.find("
+    
     raw_line = str(raw_list).split(">")
 
     desire_line = raw_line[0].split(" ")
@@ -64,7 +52,7 @@ def cleanLink(raw_list):
 
     #       ".mp4" link <str>, next-post url <str>.
     return dict_line['value'], dict_line['href']
-
+'''
 
 #print(dict_line)
 #print(dict_line['value'])
@@ -87,7 +75,7 @@ class Serie():
 
         self.cartoon = ''  # <type str>
         self.ep_links= ()  # <type tuple of dicts>
-        self.episodes= {}  # <type set of objects>
+        self.episodes= []  # <type set of objects>
 
     def addEpisode(self, yes):
         a = list(self.episodes).append(yes)
@@ -107,7 +95,7 @@ class Serie():
             #self.next = dictionary['href']
 
 
-def createDict(lista):
+def clean4Dict(lista):
     #	creates dictionary:
     dict_line = {}
     aux = []
@@ -120,7 +108,7 @@ def createDict(lista):
     return dict_line
 
 
-def createLink(raw_list):
+def createDict(raw_list):
     '''raw_ soup.find("
     '''
     raw_line = str(raw_list).split(">")
@@ -128,12 +116,13 @@ def createLink(raw_list):
     desire_line = raw_line[0].split(" ")
     del desire_line[0]  # deletes "<dType"
 
-    dict_line = createDict(desire_line)
+    dict_line = clean4Dict(desire_line)
     print(dict_line)
     #       ".mp4" link <str>, next-post url <str>.
-    
+
     # TODO method para hallar el next-post.
-    return dict_line['value'], dict_line['href']
+    #return dict_line['value'], dict_line['href']
+    return dict_line  #dictionary
 
 
 def webLink(web_link):
@@ -142,7 +131,16 @@ def webLink(web_link):
     soup = BeautifulSoup(r.content, "html.parser")
     main_in = soup.find("input", attrs={"type": "hidden"})
 
-    return createLink(main_in)
+    nextpost=  soup.find("a", attrs={"class": "next"})  # TODO NEXT POST)
+
+    return createDict(main_in)["value"], createDict(nextpost)["href"]
+
+
+def linkNext(post):
+
+
+    return
+
 
 # LIMPIEZA DE CAPITULOS con el mismo link del 1er capitulo.
 chapters_raw = soup.find("ul", attrs={"class": "video-series-list list-inline"})
@@ -163,33 +161,35 @@ for i in range(len(aux_crl)):
     for j in range(len(aux_crl[i])):
         aux_crl[i][j] = aux_crl[i][j].split("=")
         if j >= 1:
-            aux_crl[i][j][1] = aux_crl[i][j][1].replace('"','')
+            aux_crl[i][j][1] = aux_crl[i][j][1].replace('"', '')
         aaa.append(aux_crl[i][j])
     aa.append(aaa)
 chapters_list = []
 print("Nro de Capitulos:", len(aa))
 
+
 for z in range(len(aa) -1):
     del aa[z][0]
-    #print(aa[z])
     dict_aux = {}
-    for y in range(len(aa)):
-        dict_aux[aa[z][0][0]] = aa[z][0][1]  # href = url (link post)
-        dict_aux[aa[z][1][0]] = aa[z][1][1]  # title= name (chapter)
-        chapters_list.append(dict_aux)
 
+    dict_aux[aa[z][0][0]] = aa[z][0][1]  # href = url (link post)
+    dict_aux[aa[z][1][0]] = aa[z][1][1]  # title= name (chapter)
+    chapters_list.append(dict_aux)
 
+print(aa)
 print(chapters_list)
 
 ver = Serie()
 ver.cartoon = "Samurai Jack"
 ver.ep_links= tuple(chapters_list)
-
+print(ver.ep_links)
 for i in range(len(ver.ep_links)):
     ex_dato = ver.ep_links[i]
 
-    ver.episodes = Serie().Episode(ex_dato)
-
+    ver.episodes.append(ver.Episode(ex_dato))
+    a = ver.episodes
+    print(a[-1])
+    print(a[-1].vurl + "\t" + a[-1].name)
     #ver.ep_links[i][1] = ver.episodes[i][1]  # clean dict.
 
-print(ver.ep_links)
+    time.sleep(0.5)
